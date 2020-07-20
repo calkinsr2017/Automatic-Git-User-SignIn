6358cefc8c6e3e59f57502c4672dc2d57d5b7df2
MikhailSorokin <sorokinmike96@gmail.com>
2020-07-20 07:16:22 -0400

    Fix to adding .git at the end of the file + some userInterface cleanup.

--- switchUsers.py
+++ switchUsers.py
@@ -1,15 +1,14 @@
 import subprocess
 
-
 ssh = input("Enter your SSH key name: ")
 userName = input("Enter your github username: ")
 email = input("Enter your github email: ")
-repo = input("Paste in the github repo that you are working on: ")
+repo = input("Paste in the github repo that you are working on (with owner name!): ")
 
-fullString = "git@github-" + ssh + ":" + repo
+fullString = "git@github-" + ssh + ":" + repo + ".git"
 
 subprocess.run(["git", "remote", "set-url", "origin", fullString])
-subprocess.run(["git", "config", "user.name", userName])
-subprocess.run(["git", "config", "user.email", email])
-
-
+subprocess.run(["git", "config", "user.name", "--replace-all", userName])
+subprocess.run(["git", "config", "--global", "--replace-all", "user.name", userName])
+subprocess.run(["git", "config", "--replace-all", "user.email", email])
+subprocess.run(["git", "config", "--global", "--replace-all", "user.email", email])
