784900841da8bf3efff692a39b64135f4a1473da
calkinsr2017 <rcalkins@umd.edu>
2020-07-23 09:35:30 -0400

    Fixed issue where name appeared wrong

--- switchUsers.py
+++ switchUsers.py
@@ -8,7 +8,7 @@ repo = input("Paste in the github repo that you are working on (with owner name!
 fullString = "git@github-" + ssh + ":" + repo + ".git"
 
 subprocess.run(["git", "remote", "set-url", "origin", fullString])
-subprocess.run(["git", "config", "user.name", "--replace-all", userName])
+subprocess.run(["git", "config", "--replace-all", "user.name",  userName])
 subprocess.run(["git", "config", "--global", "--replace-all", "user.name", userName])
 subprocess.run(["git", "config", "--replace-all", "user.email", email])
 subprocess.run(["git", "config", "--global", "--replace-all", "user.email", email])
