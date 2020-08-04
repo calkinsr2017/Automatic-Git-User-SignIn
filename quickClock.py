812b8a242df6e2dbeeb938c4286efdd8d36dde92
calkinsr2017 <rcalkins@umd.edu>
2020-08-04 11:44:27 -0400

    Created a script to log you in and out of opentime clock and created the startMyWorday module to be run once before you start workin on any git project and as soon as you get into the office

---
+++ quickClock.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import getpass
+
+userName = input("Enter Time Clock User Name: ")
+password = getpass.getpass("Enter Time Clock Password: ")
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.opentimeclock.com/free.html")
+#assert "opentimeclock" in driver.title
+elem = driver.find_element_by_id("txtUser")
+elem.clear()
+elem.send_keys(userName)
+elem.send_keys(Keys.RETURN)
+
+elem = driver.find_element_by_id("txtPassword")
+elem.clear()
+elem.send_keys(password)
+elem.send_keys(Keys.RETURN)
+
+
+driver.close()
+
+
+
---
+++ startMyWorkday.py
@@ -0,0 +1,7 @@
+import subprocess
+import os
+
+
+subprocess.run(["python", "Automatic-Git-User-SignIn\switchUsers.py"])
+
+subprocess.run(["python", "Automatic-Git-User-SignIn\quickClock.py"])
